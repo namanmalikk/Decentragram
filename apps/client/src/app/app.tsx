08fabd9c06c8273682f492544630e5ca6e9f063f
Naman Malik <namanmalikk@gmail.com>
2022-02-03 18:54:29 +0530

    initial nx setup

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
-
+import Web3 from 'web3';
 export function App() {
   return (
     <>
